db524f277a1eabbbb1961616e80b3dafd1a3fdc4
Patel <j_patel73637@tss-hr420-dm29.fc.ca>
2018-11-07 11:10:38 -0500

    eg with one more vm

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ const vm = new Vue({
     el : "#app",
     data : {
         welcomeMessage : "our first vue app",
+        anothermessage : "This is HazVue",
+        
+        targetURL : "http://vuejs.org",
+        anchorOff : true,
+        hazVue : true,
 
         deliciousFruit : [
             {name: "apple", flavour: "tasty"},
@@ -10,9 +15,20 @@ const vm = new Vue({
         ]
     },
 
+    changeText(){
+        this.anothermessage = "changed this from a function call";
+    }
+
     methods:{
         logFruit(e){
             console.log(e.currentTarget);
         }
     }
 });
+
+const vm2 = new Vue({
+    el: "#footer",
+    data:{
+        footermessage : "This is footer",
+    }
+})
